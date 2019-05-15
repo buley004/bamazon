73077cfc0074c5c97666e000142f2c5aaa4d7145
buley004 <colinbuley@Colins-MBP.domain>
2019-05-14 20:11:10 -0700

    orders removing from database correctly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,6 +9,7 @@ var validIds = [];
 //store chosen item and quantity
 var id;
 var chosenQuantity;
+var orderCost;
 
 //create connection to mysql database
 var connection = mysql.createConnection({
@@ -26,7 +27,7 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   displayItems();
@@ -40,11 +41,11 @@ function runStore() {
       type: "number",
       message: "Please enter the Item ID of what you would like to purchase.",
       name: "itemId",
-      validate: function(value) {
+      validate: function (value) {
         if (validIds.includes(value) === true) {
           return true;
         }
-        console.log("\nPlease enter a valid ID"); 
+        console.log("\nPlease enter a valid ID");
         return false;
       }
     },
@@ -52,7 +53,7 @@ function runStore() {
       type: "number",
       message: "How many would you like to buy?",
       name: "quantity",
-      validate: function(value) {
+      validate: function (value) {
         if (Number.isInteger(value) === true) {
           return true;
         }
@@ -60,7 +61,7 @@ function runStore() {
         return false;
       }
     }
-  ]).then(function(res){
+  ]).then(function (res) {
     id = res.itemId;
     chosenQuantity = res.quantity;
 
@@ -69,8 +70,8 @@ function runStore() {
   });
 };
 
-function displayItems(){
-  connection.query("SELECT * FROM products", function(err, results) {
+function displayItems() {
+  connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
 
     //display store stock
@@ -88,10 +89,41 @@ function displayItems(){
 };
 
 function checkStock(id, quant) {
-  connection.query("SELECT * FROM products WHERE item_id = " + id, function(err, results) {
-    if (err) throw err; 
+  connection.query("SELECT * FROM products WHERE item_id = " + id, function (err, results) {
+    if (err) throw err;
     console.log(results);
-    
-  })
-}
+
+    //display insufficient stock message if order too large
+    if (quant > results[0].stock_quantity) {
+      console.log("Insufficient quantity");
+    }
+    else {
+      orderCost = quant * results[0].price;
+      var remainingUnits = results[0].stock_quantity - quant;
+      console.log("\nTotal Cost: " + orderCost);
+      console.log("\nRemaining: " + remainingUnits);
+      //remove items from stock table
+      updateStock(id, remainingUnits);
+    };
+
+  });
+};
+
+function updateStock(id, quant) {
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: quant
+      },
+      {
+        item_id: id
+      }
+    ],
+    function (error) {
+      if (error) throw err;
+      console.log("Bid placed successfully!");
+    }
+  );
+};
 
